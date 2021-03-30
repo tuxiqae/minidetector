9b4efba9558b417c5abfbe008afd4638ca7762e6
Sagi Sarussi <sagis@sagis.dev>
2021-03-30 22:58:01 +0300

    Change the logic so the sniffer won't open new DB sessions for each packet Add type hints

--- minidetector/main.py
+++ minidetector/main.py
@@ -2,44 +2,50 @@
 import argparse
 import threading
 import logging
+import typing
 
 from scapy.sendrecv import sniff
 from scapy.layers.inet import IP
 from scapy.layers.l2 import Ether
+from scapy.packet import Packet
+from typing import Set
 
-from .database import create_tables, Entity, create_session, drop_tables
+from .database import create_tables, Entity, create_session, drop_tables, load_db_entries
 from queue import Queue
 
 packet_queue: Queue = Queue()
 
 
-def on_packet(p):
+def on_packet(p: Packet) -> None:
     if Ether not in p or IP not in p:
         return
     packet_queue.put(p)
 
 
-def process_data():
+def process_data() -> None:
+    ip_mac_set: typing.Set[typing.Tuple[str, str]] = load_db_entries()
+
     packet_count = 0
+    session = create_session()  # Creates a new DB session
     while packet := packet_queue.get():
         packet_count += 1
-        if packet_count % 100 == 0:
-            logging.info(f'Queue size: {packet_queue.qsize()}')
+        if packet_count % 100 == 0 and (qs := packet_queue.qsize() or 0) != 0:
+            logging.info(f'Queue size: {qs}')
         mac = packet[Ether].src
         ip = packet[IP].src
-        session = create_session()  # DB session
-        query = session.query(Entity).filter_by(mac=mac, ip=ip)
-        if query.count() > 0:
-            logging.debug(f'skipping packet {ip} {mac}')
-            continue
-        entity = Entity(mac=mac, ip=ip)
-        session.add(entity)
-        session.commit()
-        logging.info(f'Added entity {entity}')
-        session.close()
+
+        if (pair := (ip, mac)) not in ip_mac_set:
+            ip_mac_set.add(pair)
+
+            entity = Entity(mac=mac, ip=ip)
+            session.add(entity)
+            session.commit()
+
+            logging.info(f'Added entity {entity}')
+    session.close()
 
 
-def main():
+def main() -> None:
     parser = argparse.ArgumentParser(
         description='minidetector is an example tool '
                     'for detecting network identities and inserting them into a PostgreSQL database')

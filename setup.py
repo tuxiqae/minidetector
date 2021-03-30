4a61742dbde1522e1cba52ead12402ad1ba82d6c
Sagi Sarussi <sagis@sagis.dev>
2021-03-30 15:03:21 +0300

    - Add fastapi and uvicorn as dependencies. - Constrain minidetector to only use older versions of Python due to an issue with version 2.4.4 of scapy

--- setup.py
+++ setup.py
@@ -5,18 +5,25 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="minidetector",
-    version="0.0.1",
+    version="0.0.2",
     author="Amit Itzkovitch",
     author_email="amit7itz@gmail.com",
     description="A small example package",
     long_description=long_description,
     long_description_content_type="text/markdown",
     packages=setuptools.find_packages(),
-    install_requires=['scapy', 'sqlalchemy', 'psycopg2'],
+    install_requires=[
+        "scapy",
+        "sqlalchemy>=1.3,<1.4",
+        "psycopg2",
+        "fastapi",
+        "uvicorn[standard]"
+    ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.8',
+    python_requires='>=3.6, <3.8.8',
+    # Due to issues with `scapy` v2.4.4, minidetector doesn't work with newer versions of Python such as Python 3.8.8, 3.9
 )

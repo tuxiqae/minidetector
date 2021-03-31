32948fb547795a6f9085a484c1a3bde8f1e6ec71
Sagi Sarussi <sagis@sagis.dev>
2021-03-31 16:47:30 +0300

    Implement the REST API
    The API is built upon 3 endpoints:
    - GET /all - Returns all entities from the DB.
    - GET /routers - Returns all routers from the DB (MAC addresses which appeared more than 3 times).
    - GET /lastseen - Returns all entities from the DB, ordered by recency

---
+++ api/main.py
@@ -0,0 +1,59 @@
+import uvicorn
+from fastapi import Depends, FastAPI, HTTPException
+from lib.database import fetch_entities, create_session, Session, fetch_routers, fetch_lastseen
+from fastapi.encoders import jsonable_encoder
+from fastapi.responses import JSONResponse
+
+app = FastAPI()
+
+
+# Dependency
+def get_db():
+    db = create_session()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+@app.get("/all")
+def get_entities(db: Session = Depends(get_db)):
+    response = {"data": [], "err": None}
+
+    try:
+        response["data"] = [{"ip": ip, "mac": mac} for mac, ip in fetch_entities(db).all()]
+    except BaseException as e:
+        response["err"] = str(e)
+        raise HTTPException(status_code=500, detail=JSONResponse(content=jsonable_encoder(response)))
+
+    return JSONResponse(content=jsonable_encoder(response))
+
+
+@app.get("/routers")
+def get_routers(db: Session = Depends(get_db)):
+    response = {"data": [], "err": None}
+
+    try:
+        response["data"] = [macs[0] for macs in fetch_routers(db).all()]
+    except BaseException as e:
+        response["err"] = str(e)
+        raise HTTPException(status_code=500, detail=JSONResponse(content=jsonable_encoder(response)))
+
+    return JSONResponse(content=jsonable_encoder(response))
+
+
+@app.get("/lastseen")
+def get_lastseen(db: Session = Depends(get_db)):
+    response = {"data": [], "err": None}
+
+    try:
+        response["data"] = [{"ip": ip, "mac": mac} for mac, ip in fetch_lastseen(db).all()]
+    except BaseException as e:
+        response["err"] = str(e)
+        raise HTTPException(status_code=500, detail=JSONResponse(content=jsonable_encoder(response)))
+
+    return JSONResponse(content=jsonable_encoder(response))
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)

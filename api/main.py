b18bac730d39f228129f3410609cfa221d9a9602
Sagi Sarussi <sagis@sagis.dev>
2021-03-31 19:10:36 +0300

    Add the `timestamp` to the GET `/lastseen` endpoint Add logging and error handling

--- api/main.py
+++ api/main.py
@@ -1,4 +1,6 @@
 import uvicorn
+import logging
+
 from fastapi import Depends, FastAPI, HTTPException
 from lib.database import fetch_entities, create_session, Session, fetch_routers, fetch_lastseen
 from fastapi.encoders import jsonable_encoder
@@ -24,7 +26,8 @@ def get_entities(db: Session = Depends(get_db)):
         response["data"] = [{"ip": ip, "mac": mac} for mac, ip in fetch_entities(db).all()]
     except BaseException as e:
         response["err"] = str(e)
-        raise HTTPException(status_code=500, detail=JSONResponse(content=jsonable_encoder(response)))
+        logging.error(e)
+        raise HTTPException(status_code=500, detail=response)
 
     return JSONResponse(content=jsonable_encoder(response))
 
@@ -36,8 +39,9 @@ def get_routers(db: Session = Depends(get_db)):
     try:
         response["data"] = [macs[0] for macs in fetch_routers(db).all()]
     except BaseException as e:
+        logging.error(e)
         response["err"] = str(e)
-        raise HTTPException(status_code=500, detail=JSONResponse(content=jsonable_encoder(response)))
+        raise HTTPException(status_code=500, detail=response)
 
     return JSONResponse(content=jsonable_encoder(response))
 
@@ -47,10 +51,11 @@ def get_lastseen(db: Session = Depends(get_db)):
     response = {"data": [], "err": None}
 
     try:
-        response["data"] = [{"ip": ip, "mac": mac} for mac, ip in fetch_lastseen(db).all()]
+        response["data"] = [{"timestamp": ts, "ip": ip, "mac": mac} for ts, mac, ip in fetch_lastseen(db).all()]
     except BaseException as e:
         response["err"] = str(e)
-        raise HTTPException(status_code=500, detail=JSONResponse(content=jsonable_encoder(response)))
+        logging.error(e)
+        raise HTTPException(status_code=500, detail=response)
 
     return JSONResponse(content=jsonable_encoder(response))
 

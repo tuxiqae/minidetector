85772958cc80da8bf4af6ad10cb47ed8073478f7
Sagi Sarussi <sagis@sagis.dev>
2021-04-01 14:26:53 +0300

    Change the default entrypoint to the Swagger docs Add OpenAPI tags Add error handling for the API_PORT environment variable

--- api/main.py
+++ api/main.py
@@ -1,6 +1,5 @@
 import logging
 import uvicorn
-
 from fastapi import FastAPI
 from os import environ
 
@@ -9,11 +8,21 @@ from endpoints.routers import router as routers_router
 from endpoints.lastseen import router as lastseen_router
 from lib.ascii_banner import print_ascii_banner
 
-app = FastAPI()
+app = FastAPI(docs_url="/",
+              redoc_url="/redoc",
+              openapi_tags=[{"name": "Entities", "description": "Fetch entities from the DB"}])
+
 app.include_router(all_router)
 app.include_router(routers_router)
 app.include_router(lastseen_router)
 
 if __name__ == "__main__":
     print_ascii_banner("MiniDetectorAPI")
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    try:
+        port = environ["API_PORT"]
+        uvicorn.run(app, host="0.0.0.0", port=int(port))
+    except KeyError:
+        raise KeyError("Could not fetch the environment variable `API_PORT`")
+    except BaseException as e:
+        logging.critical(e)
+        exit(1)

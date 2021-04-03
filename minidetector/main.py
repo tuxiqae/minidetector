7a45708827def03de50241a01cd3456c0b7a8f45
Sagi Sarussi <sagis@sagis.dev>
2021-04-03 18:58:55 +0300

    Fix import

--- minidetector/main.py
+++ minidetector/main.py
@@ -6,7 +6,7 @@ from scapy.sendrecv import sniff
 from lib.ascii_banner import print_ascii_banner
 from lib.database import db_init
 from argparser import argparse_init
-from minidetector.packet_processor import on_packet, process_data
+from packet_processor import on_packet, process_data
 
 
 def main() -> None:

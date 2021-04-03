a70164b83646f3c7758a2878ef7cfdcb2bc3cbfc
Sagi Sarussi <sagis@sagis.dev>
2021-04-03 18:59:31 +0300

    Organize the code

--- api/main.py
+++ api/main.py
@@ -8,21 +8,24 @@ from endpoints.routers import router as routers_router
 from endpoints.lastseen import router as lastseen_router
 from lib.ascii_banner import print_ascii_banner
 
-app = FastAPI(docs_url="/",
-              redoc_url="/redoc",
-              openapi_tags=[{"name": "Entities", "description": "Fetch entities from the DB"}])
-
-app.include_router(all_router)
-app.include_router(routers_router)
-app.include_router(lastseen_router)
-
 if __name__ == "__main__":
     print_ascii_banner("MiniDetectorAPI")
+
+    app = FastAPI(docs_url="/",
+                  redoc_url="/redoc",
+                  openapi_tags=[{"name": "Entities", "description": "Fetch entities from the DB"}])
+
+    app.include_router(all_router)
+    app.include_router(routers_router)
+    app.include_router(lastseen_router)
+
     try:
         port = environ["API_PORT"]
-        uvicorn.run(app, host="0.0.0.0", port=int(port))
     except KeyError:
         raise KeyError("Could not fetch the environment variable `API_PORT`")
+
+    try:
+        uvicorn.run(app, host="0.0.0.0", port=int(port))
     except Exception as e:
         logging.exception(e)
         exit(1)
